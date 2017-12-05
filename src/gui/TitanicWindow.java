92e2a8ebf76ebf2b12287a3e51cf069b082f1dc1
dosterji <dosterji@plu.edu>
2017-12-05 14:21:01 -0800

    Got some basic GUI stuff done.

--- src/gui/TitanicWindow.java
+++ src/gui/TitanicWindow.java
@@ -2,20 +2,23 @@ package gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * This class holds all of the other gui components
  */
 public class TitanicWindow extends JFrame {
 
-    private ImageIcon shipImage;
-    private JButton startButton, predictButton;
-    private JLabel titleLabel;
-    private JTextField proportionField, inputField;
-    private JTextArea resultsDisplay;
+    private ImageIcon shipImage;        //Image of a boat
+    private JButton predictButton;      //The button that does the prediction
+    private JLabel titleLabel;          //The title label
+    private JLabel messageLabel;        //The label that tells the user what's going on
+    private JLabel textFieldLabel;      //The label for the text field
+    private JTextField inputField;      //The textField
 
     public TitanicWindow() {
-        this.setSize(500, 500);
+        this.setSize(600, 600);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Titanic");
 
@@ -25,9 +28,9 @@ public class TitanicWindow extends JFrame {
 
         titleLabel = new JLabel("  WHO WILL SURVIVE?", shipImage, SwingConstants.CENTER);
         titleLabel.setFont(f);
+        messageLabel = new JLabel("Enter your data to see survival prediction");
+        textFieldLabel = new JLabel("Your Data: ");
 
-        proportionField = new JTextField(20);
-        startButton = new JButton("Train");
 
         inputField = new JTextField(20);
 
@@ -36,15 +39,25 @@ public class TitanicWindow extends JFrame {
 
         JPanel panel = new JPanel();
         panel.add(titleLabel);
-        panel.add(startButton);
+        panel.add(textFieldLabel);
+        panel.add(inputField);
+        panel.add(predictButton);
 
         JPanel panelS = new JPanel();
-        panelS.add(inputField);
-        panelS.add(predictButton);
+        panelS.add(messageLabel);
+        panelS.setBackground(Color.YELLOW);
 
         this.add(panel);
         this.add(panelS, BorderLayout.SOUTH);
         this.setVisible(true);
+    }
+
+    private class ButtonListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
     }
 
     public static void main(String[] args) {
